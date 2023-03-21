e01c0ed81e01ca65036f3008215093a77415ff1c
MuXu-0212 <109685414+MuXu-0212@users.noreply.github.com>
2023-03-21 20:59:33 +0800

    Add files via upload

---
+++ 共享内存/head.h
@@ -0,0 +1,20 @@
+#ifndef __HEAD_H_
+#define __HEAD_H_
+
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <signal.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <errno.h>
+#include <sys/msg.h>
+#include <sys/sem.h>
+#include <pthread.h>
+
+#endif
