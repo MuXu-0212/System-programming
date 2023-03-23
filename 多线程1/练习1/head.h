fa68cf323aad2ab5f13fa0ae34c7821e0aef558f
MuXu-0212 <109685414+MuXu-0212@users.noreply.github.com>
2023-03-23 20:44:54 +0800

    Add files via upload

---
+++ 多线程1/练习1/head.h
@@ -0,0 +1,22 @@
+#ifndef __HEAD_H_
+#define __HEAD_H_
+
+#include <sys/types.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/ipc.h>
+#include <errno.h>
+#include <sys/shm.h>
+#include <string.h>
+#include <sys/sem.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <pthread.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+
+
+#endif
