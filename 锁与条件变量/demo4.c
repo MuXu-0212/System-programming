c996e38f7f7863934064caf17a5106f001e2ac5d
MuXu-0212 <109685414+MuXu-0212@users.noreply.github.com>
2023-04-01 23:05:22 +0800

    Add files via upload

---
+++ 锁与条件变量/demo4.c
@@ -0,0 +1,103 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <string.h>
+#include <strings.h>
+#include <errno.h>
+
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <semaphore.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+
+#define PATH "/home/gec"
+
+sem_t *data;
+sem_t *space;
+char *shm_addr;
+int shm_id;
+
+void func(int sig)
+{
+    sem_close(data);
+    sem_close(space);
+
+    shmdt(shm_addr);
+    shmctl(shm_id,IPC_RMID,NULL);
+    sem_unlink("data");
+    sem_unlink("space");
+}
+
+void func1(void)
+{
+    sem_close(data);
+    sem_close(space);
+
+    shmdt(shm_addr);
+    shmctl(shm_id,IPC_RMID,NULL);
+    sem_unlink("data");
+    sem_unlink("space");
+}
+
+
+int main(int argc, char const *argv[])
+{
+
+    shm_id = shmget(ftok(PATH,3), 1024, IPC_CREAT | IPC_EXCL | 0777);
+    if (shm_id == -1)
+    {
+        if (errno == EEXIST)
+        {
+            shm_id = shmget(ftok(PATH,3), 1024, 0777);
+        }
+        else
+        {
+            perror("申请共享内存失败\n");
+            return -1;
+        }
+
+    }
+
+    shm_addr = shmat(shm_id,NULL,0);
+    if (shm_addr == NULL)
+    {
+        perror("映射内存失败\n");
+        return -2;
+    }
+
+    //申请一个信号量,该信号量代表着数据,因为一开始我们是没有数据的,所以初始为0
+    data = sem_open("/data", O_CREAT ,0777, 0);
+    if(data == SEM_FAILED)
+    {
+        printf("申请信号量data失败.\n");
+        return -3;
+    }
+    
+    //申请一个信号量,该信号量代表着数据的使用权,如果我们把数据放到空间中,该空间的资源会-1;
+    space = sem_open("/space", O_CREAT , 0777, 1);
+    if(space == SEM_FAILED)
+    {
+        printf("申请信号量space失败.\n");
+        return -3;
+    }
+
+    while(1)
+    {
+        //申请空间写入数据
+        sem_wait(data);
+        printf("读取到的数据为:%s\n",shm_addr );
+        sem_post(space);
+        if (strncmp(shm_addr,"over",4) == 0)
+        {
+            break;
+        }
+    }
+
+    func1();
+	
+    return 0;
+}
