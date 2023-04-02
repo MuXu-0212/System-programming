d4e49de81840c6dd5dabfb686a51880b52be1f0c
MuXu-0212 <109685414+MuXu-0212@users.noreply.github.com>
2023-04-02 09:12:02 +0800

    Add files via upload

---
+++ 锁与条件变量/head.h
@@ -0,0 +1,19 @@
+#ifndef __HEAD_H_
+#define __HEAD_H_
+
+#include <sys/types.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/ipc.h>
+#include <errno.h>
+#include <sys/shm.h>
+#include <string.h>
+#include <sys/sem.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <pthread.h>
+
+
+#endif
